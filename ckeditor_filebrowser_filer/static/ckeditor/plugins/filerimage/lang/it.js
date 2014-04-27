ec9be29c07fcb465cf9ed3079c457871724ac979
Iacopo Spalletti <i.spalletti@nephila.it>
2014-04-27 07:47:57 +0200

    Add translations

---
+++ ckeditor_filebrowser_filer/static/ckeditor/plugins/filerimage/lang/it.js
@@ -0,0 +1,17 @@
+﻿CKEDITOR.plugins.setLang( 'filerimage', 'it', {
+	name: 'Inserisci immagine',
+	edit: 'Modifica image',
+	title: 'Proprietà immagine',
+	titleBasic: 'Impostazioni di base',
+	titleAdvanced: 'Impostazioni avanzate',
+	noFileAlt: 'Nessun file selezionato',
+	browse: 'Cerca',
+	clear: 'Pulisci',
+	url: 'URL',
+	caption: 'Didascalia',
+	useOriginal: 'Usa immagine originale',
+	crop: 'Ritaglia',
+	upscale: 'Ingrandisci',
+	autoscale: 'Riscalatura automatica',
+	alt: 'Testo alternativo'
+} );
