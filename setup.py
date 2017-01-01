6412b4168c48ae971c35a24c97c911ea25ce8ac5
Iacopo Spalletti <i.spalletti@nephila.it>
2017-01-01 22:51:04 +0100

    Release 0.2.1

--- setup.py
+++ setup.py
@@ -42,7 +42,7 @@ setup(
     zip_safe=False,
     keywords='django-ckeditor-filebrowser-filer, django, filer, ckeditor, filebrowser',
     classifiers=[
-        'Development Status :: 2 - Pre-Alpha',
+        'Development Status :: 5 - Production/Stable',
         'Framework :: Django',
         'Intended Audience :: Developers',
         'License :: OSI Approved :: BSD License',
